b3bd91680755a844c5f40bbbe669afb4ee3b8a2d
Brahim Jarrachi Khayya <s.khayya@gmail.com>
2025-05-09 22:58:24 +0200

    Minor modification

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,27 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))  # project root on PYTHONPATH
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-project = 'FaceBox'
-author = 'Your Name'
-release = '0.1.0'
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-]
+project = 'Facebox'
+copyright = '2025, Brahim JARRACHI KHAYYA and Ismail KHLIFI TAGZOUTI'
+author = 'Brahim JARRACHI KHAYYA and Ismail KHLIFI TAGZOUTI'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'sphinx_rtd_theme'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
 html_static_path = ['_static']
